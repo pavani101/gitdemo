8dd5ad3f7b951568719652106f3b10dbafacdefd
pavani101 <56316598+pavani101@users.noreply.github.com>
2023-09-10 22:55:15 -0500

    Update Project1.java

--- src/main/java/com/mycompany/project1/Project1.java
+++ src/main/java/com/mycompany/project1/Project1.java
@@ -11,6 +11,6 @@ package com.mycompany.project1;
 public class Project1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Huntsville!");
     }
 }
